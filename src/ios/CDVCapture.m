a1402b4f8e2463da2ae83a05c8e3e63033fc4262
Ross Martin <rmartin311@gmail.com>
2015-03-31 16:32:47 -0500

    set picker controller to UIImagePickerControllerQualityTypeHigh

--- src/ios/CDVCapture.m
+++ src/ios/CDVCapture.m
@@ -267,7 +267,7 @@
         // iOS 4.0
         if ([pickerController respondsToSelector:@selector(cameraCaptureMode)]) {
             pickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModeVideo;
-            // pickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
+            pickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
             // pickerController.cameraDevice = UIImagePickerControllerCameraDeviceRear;
             // pickerController.cameraFlashMode = UIImagePickerControllerCameraFlashModeAuto;
         }
